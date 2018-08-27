5a6a31aff4ba17cf40ea31558eb0814cd0a98c5a
Jason Schmitt <schmitty890@gmail.com>
2018-08-26 21:54:18 -0400

    adding client side js for example

---
+++ public/assets/js/grocery-list.js
@@ -0,0 +1,5 @@
+console.log('this javascript was concatenated and minified by gulp.');
+console.log('we could add more js files and they would automatically be read in with the gulp script.');
+console.log('if grocery list client side js was needed, it would go here.');
+console.log('no client side javascript was needed for this project example.');
+console.log('see https://github.com/schmitty890/bofa-2 to see a version of this with no handlebars, and client side javascript.');
