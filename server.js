3fa0ec430a4651cc7305cb32c082fddd307eb250
Jason Schmitt <schmitty890@gmail.com>
2018-08-26 21:41:26 -0400

    node server setup with handlebars

---
+++ server.js
@@ -0,0 +1,46 @@
+/**
+ * Module dependencies.
+ */
+const express = require('express');
+const bodyParser = require('body-parser');
+const chalk = require('chalk');
+const path = require('path');
+const exphbs = require('express-handlebars');
+
+/**
+ * Create Express server.
+ */
+const app = express();
+
+/**
+ * Express configuration.
+ */
+app.set('host', process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0');
+app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// use build folder that our contains bundled concatenated minified js and css files
+app.use('/build', express.static(__dirname + '/build'));
+
+// get all routes
+require('./controllers/html-routes.js')(app);
+require('./controllers/api-routes.js')(app);
+
+// show 404 page if no route has been hit
+app.get('*', function(req, res) {
+  res.send('404 page not found');
+});
+
+/**
+ * Start Express server.
+ */
+app.listen(app.get('port'), () => {
+  console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'), app.get('env'));
+  console.log('  Press CTRL-C to stop\n');
+});
